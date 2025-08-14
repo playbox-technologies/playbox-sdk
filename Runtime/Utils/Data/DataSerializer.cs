32873619a0afce8b2b60a83f7983503440f4cc6f
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-08-14 22:37:16 +0300

    Update DataSerializer.cs

--- Runtime/Utils/Data/DataSerializer.cs
+++ Runtime/Utils/Data/DataSerializer.cs
@@ -5,19 +5,32 @@ namespace Utils.Data
 {
     public static class DataSerializer
     {
+        private const byte Key = 0x11;
+
         public static byte[] Serialize(object obj)
         {
             using var memoryStream = new MemoryStream();
-            
-            new BinaryFormatter().Serialize(memoryStream,obj);
-            
-            return memoryStream.ToArray();
+#pragma warning disable SYSLIB0011
+            new BinaryFormatter().Serialize(memoryStream, obj);
+#pragma warning restore SYSLIB0011
+            byte[] data = memoryStream.ToArray();
+            Obfuscate(data);
+            return data;
         }
 
         public static T Deserialize<T>(byte[] bytes)
         {
+            Obfuscate(bytes);
             using var memoryStream = new MemoryStream(bytes);
+#pragma warning disable SYSLIB0011
             return (T)new BinaryFormatter().Deserialize(memoryStream);
+#pragma warning restore SYSLIB0011
+        }
+
+        private static void Obfuscate(byte[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+                data[i] ^= Key; 
         }
     }
 }
