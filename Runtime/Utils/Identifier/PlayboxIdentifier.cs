e9a94a76bbf29d0f82b2f9cf8402915399f32015
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-09-19 16:24:38 +0300

    Add guid generator

---
+++ Runtime/Utils/Identifier/PlayboxIdentifier.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using UnityEngine;
+
+namespace Utils.Identifier
+{
+    public static class PlayboxIdentifier
+    {
+        public static string GetUID => GetActualGUID();
+
+        private static readonly string GuidKey = "Playbox_GUID";
+        
+        public static string NewGUID()
+        {
+            JObject obj = new JObject();
+            
+            obj["generated_uuid"] = Guid.NewGuid().ToString();
+            obj["device_id"] = SystemInfo.deviceUniqueIdentifier;
+            obj["device_model"] = SystemInfo.deviceModel;
+            obj["device_name"] = SystemInfo.deviceName;
+            obj["register_date"] = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+            
+            return obj.ToString();
+        }
+
+        public static string GetActualGUID()
+        {
+            if (PlayerPrefs.HasKey(GuidKey))
+            {
+                return PlayerPrefs.GetString(GuidKey);
+            }
+            
+            var guid = NewGUID();
+            
+            PlayerPrefs.SetString(GuidKey, guid);
+            
+            PlayerPrefs.Save();
+            
+            return guid;
+        }
+    }
+}
