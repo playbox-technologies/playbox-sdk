5f68f73e54b46bd103629f6d800422a1913fd58f
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-11-07 18:44:09 +0300

    Update CrossPromoButton.cs

--- Runtime/Any/Scripts/CrossPromo/CrossPromoButton.cs
+++ Runtime/Any/Scripts/CrossPromo/CrossPromoButton.cs
@@ -6,24 +6,11 @@ namespace Playbox
 {
     public class CrossPromoButton : MonoBehaviour
     {
-        [SerializeField] private string AfPromoteLink = "https://dreamsim.onelink.me/4DPE?af_xp=app&pid=Cross_sale&c=R2R_campaign&af_siteid=R2R_campaign";
+        [SerializeField] private string AfPromoteLink = "";
         
         [SerializeField] private string androidPromoteId = "";
 
         [SerializeField] private string iosPromoteId = "";
-
-        private string promotedID =>
-
-#if UNITY_IOS
-
-            iosPromoteId
-
-#elif UNITY_ANDROID
-
-        androidPromoteId
-
-#endif
-        ;
 
         [SerializeField] private string campaign = "";
 
@@ -46,40 +33,20 @@ namespace Playbox
 
         public void RecordCrossPromoImpression()
         {
-            Dictionary<string, string> properties = new();
+            //Dictionary<string, string> properties = new();
 
-            properties.Add("campaign", campaign);
-            properties.Add("promoted_id", promotedID);
-            properties.Add("af_siteid", Application.identifier);
-            properties.Add("af_sub1", Application.identifier);
+            //properties.Add("campaign", campaign);
+           // properties.Add("promoted_id", promotedID);
+           //properties.Add("af_siteid", Application.identifier);
+            //properties.Add("af_sub1", Application.identifier);
 
-            CrossPromo.RecordCrossPromoImpression(promotedID, campaign, properties);
+            //CrossPromo.RecordCrossPromoImpression(promotedID, campaign, properties);
         }
 
         public void Click()
         {
-            Dictionary<string, string> properties = new();
-
-            properties.Add("campaign", campaign);
-            properties.Add("promoted_id", promotedID);
-            properties.Add("af_siteid", Application.identifier);
-            properties.Add("af_sub1", Application.identifier);
-
             CrossPromo.OpenStore(AfPromoteLink);
-            
-            promotedID.PlayboxInfo();
         }
-
-        public void GenerateLink()
-        {
-            Dictionary<string, string> properties = new();
-
-            properties.Add("campaign", campaign);
-            properties.Add("promoted_id", promotedID);
-            properties.Add("af_siteid", Application.identifier);
-            properties.Add("af_sub1", Application.identifier);
-
-            CrossPromo.GenerateUserInviteLink(properties);
-        }
+        
     }
 }
