912bf9cfc3acebd9c60224747e22fb9c8c6b9630
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-12-01 21:31:04 +0300

    Increment and add ProductDataAdapter

--- Runtime/Any/Scripts/Analytics/Analytics.cs
+++ Runtime/Any/Scripts/Analytics/Analytics.cs
@@ -21,15 +21,6 @@ using Firebase.Crashlytics;
 
 namespace Playbox
 {
-    public class ProductDataAdapter
-    {
-        public string TransactionId { get; set; }
-        public string DefinitionId { get; set; }
-        public decimal MetadataLocalizedPrice { get; set; }
-        public string MetadataIsoCurrencyCode { get; set; }
-        public string Receipt { get; set; }
-    }
-    
     /// <summary>
     /// Clavicular static analytics collection class
     /// </summary>
---
+++ Runtime/Any/Scripts/Analytics/ProductDataAdapter.cs
@@ -0,0 +1,11 @@
+﻿namespace Playbox
+{
+    public class ProductDataAdapter
+    {
+        public string TransactionId { get; set; }
+        public string DefinitionId { get; set; }
+        public decimal MetadataLocalizedPrice { get; set; }
+        public string MetadataIsoCurrencyCode { get; set; }
+        public string Receipt { get; set; }
+    }
+}
