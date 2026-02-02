e55a0248146063ca558791f541920f4dae39aab3
Sergiychuk Viachaslau <qertysig@gmail.com>
2026-02-02 19:13:47 +0300

    add emulator detector

---
+++ Runtime/Any/Scripts/DeviceDevector/DeviceDetector.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Device;
+
+namespace Playbox.DeviceDevector
+{
+    public static class DeviceDetector
+    {
+        public static bool ValidateGPU()
+        {
+            return SystemInfo.graphicsDeviceName.ToLower().Contains("SwiftShader".ToLower());
+        }
+
+        public static bool ValidateCPU()
+        {
+           return SystemInfo.processorCount < 4;
+        }
+        
+        public static bool IsTestDevice() => 
+            ValidateGPU() || 
+            ValidateCPU();
+    }
+}
--- Runtime/Any/Scripts/Initializations/DevToDevInitialization.cs
+++ Runtime/Any/Scripts/Initializations/DevToDevInitialization.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using DevToDev.Analytics;
+using Playbox.DeviceDevector;
 using Playbox.SdkConfigurations;
 using UnityEngine;
 
@@ -50,17 +51,62 @@ namespace Playbox
 
         IEnumerator SendUserCard()
         {
-            
             yield return null;
             yield return new WaitForEndOfFrame();
             
-            DTDUserCard.Set("device_ram", SystemInfo.systemMemorySize);
-            DTDUserCard.Set("gpu_memory", SystemInfo.graphicsMemorySize);
-            DTDUserCard.Set("screen_width", Screen.width);
-            DTDUserCard.Set("screen_height", Screen.height);
+            //Build
+            DTDUserCard.Set("is_debug_build", Debug.isDebugBuild ? 1 : 0);
+
+            
+            //Cores
+            DTDUserCard.Set("cpu_cores", SystemInfo.processorCount);
+            DTDUserCard.Set("cpu_freq", SystemInfo.processorFrequency);
+            DTDUserCard.Set("cpu_model", SystemInfo.processorModel);
+            DTDUserCard.Set("cpu_type", SystemInfo.processorType);
+            DTDUserCard.Set("cpu_manufacturer", SystemInfo.processorManufacturer);
+            
+            
+            //Names
             DTDUserCard.Set("device_name", SystemInfo.deviceName);
+            DTDUserCard.Set("device_model", SystemInfo.deviceModel);
             DTDUserCard.Set("graphicsDeviceName", SystemInfo.graphicsDeviceName);
             DTDUserCard.Set("graphicsDeviceType", SystemInfo.graphicsDeviceType.ToString());
+            
+            //MEMORY
+            DTDUserCard.Set("device_ram", SystemInfo.systemMemorySize);
+            DTDUserCard.Set("gpu_memory", SystemInfo.graphicsMemorySize);
+            DTDUserCard.Set("system_memory", SystemInfo.systemMemorySize);
+            
+            //virtual screen(UI)
+            DTDUserCard.Set("virtual_screen_width", Screen.width);
+            DTDUserCard.Set("virtual_screen_height", Screen.height);
+            DTDUserCard.Set("screen_safeArea_height", Screen.safeArea.height);
+            DTDUserCard.Set("screen_safeArea_width", Screen.safeArea.width);
+            
+            //Physical Screen(Device pixels)
+            DTDUserCard.Set("physical_screen_height", Screen.currentResolution.height);
+            DTDUserCard.Set("physical_screen_width", Screen.currentResolution.width);
+            DTDUserCard.Set("physical_screen_dpi", Screen.dpi);
+            
+            //Versions
+            DTDUserCard.Set("graphics_device_version", SystemInfo.graphicsDeviceVersion);
+            DTDUserCard.Set("graphics_shader_level", SystemInfo.graphicsShaderLevel);
+            
+            //Quality Levels
+            DTDUserCard.Set("quality_level", QualitySettings.GetQualityLevel());
+            DTDUserCard.Set("vSync_count", QualitySettings.vSyncCount);
+            DTDUserCard.Set("target_fps", Application.targetFrameRate);
+            DTDUserCard.Set("color_space", QualitySettings.activeColorSpace.ToString());
+            DTDUserCard.Set("render_pipeline", QualitySettings.renderPipeline.name);
+            
+          
+
+#if UNITY_ANDROID
+            var isEmu = DeviceDetector.IsTestDevice();
+            
+            DTDUserCard.Set("is_test_device", isEmu ? 1 : 0);
+            DTDUserCard.Set("emulator_reason", isEmu ? "swiftshader_or_lowcpu" : "none");
+#endif
         }
 
         public override void Close()
