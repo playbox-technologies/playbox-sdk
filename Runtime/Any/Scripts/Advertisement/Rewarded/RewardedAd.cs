9424aee52f89cf561cd49845de83220f50d2a6be
Sergiychuk Viachaslau <qertysig@gmail.com>
2026-02-17 12:35:46 +0300

    Ad new Rewarded class

---
+++ Runtime/Any/Scripts/Advertisement/Rewarded/RewardedAd.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Playbox
+{
+    public static class RewardedAd
+    {
+        public static bool IsReady { get; private set; } = false;
+        
+        private static string _unitId;
+
+        private static bool IsInitialized => MaxSdk.IsInitialized();
+        private static void LoadAd() => MaxSdk.LoadRewardedAd(_unitId);
+        private static bool isLoaded => MaxSdk.IsRewardedAdReady(_unitId);
+        private static void ShowAd(string placement) => MaxSdk.ShowRewardedAd(_unitId, placement);
+
+        private static MonoBehaviour _context;
+
+        public static void RegisterUnitID(string androidID, string iOSID, MonoBehaviour context)
+        {
+            _unitId = Application.platform switch
+            {
+                RuntimePlatform.Android => androidID,
+                RuntimePlatform.IPhonePlayer => iOSID,
+                _ => iOSID
+            };
+
+            _context = context;
+        }
+
+        public static void Load()
+        {
+            if(IsReady)
+                return;
+            
+            if(IsInitialized)
+                LoadAd();
+        }
+        
+        public static void Show(string placement = "default") => ShowAd(placement);
+
+        private static void HealthCheck()
+        {
+            IsReady = IsInitialized && isLoaded;
+        }
+    }
+}
